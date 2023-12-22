b8f8b81915984abab147ef05fc914e893640ca06
aditya98899 <adiverma9889@gmail.com>
2023-12-22 18:34:24 +0530

    add all products

--- django_dukan/urls.py
+++ django_dukan/urls.py
@@ -8,7 +8,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
+    path('shop/', include('shop.urls')),
+    path ('', include('home.urls')),
     
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
